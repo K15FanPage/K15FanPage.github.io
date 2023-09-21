4ed7e0bb36263582acaa774549694a8c8af19e40
xkirilskills <145614983+xkirilskills@users.noreply.github.com>
2023-09-22 01:39:58 +0200

    novi saundove u pokusaj

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function playSound(x) {
-    let sound = document.getElementsByTagName("audio");
-    for(let i=0; i<sound.length; i++) {
-        if(x-1==i) {
-            sound[i].play();
-            sound[i].volume = "0.2";
-        }
-
-        else {
-            sound[i].pause();
-        }
+  let sound = document.getElementsByTagName("audio");
+  for (let i = 0; i < sound.length; i++) {
+    if (x - 1 == i) {
+      sound[i].volume = "0.2";
+      sound[i].play();
+    } else {
+      sound[i].pause();
     }
+  }
 }
