dbb2c0983a0262451a496f4daa8618ca2729700d
Evgenij31 <109481850+Evgenij31@users.noreply.github.com>
2023-09-21 02:14:47 +0200

    saun печ

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function playSound(x) {
     for(let i=0; i<sound.length; i++) {
         if(x-1==i) {
             sound[i].play();
+            sound[i].volume = "0.2";
         }
 
         else {
