25c48ce5d98fa9fc363e237fe983547109025f01
Evgenij31 <109481850+Evgenij31@users.noreply.github.com>
2023-09-25 00:16:42 +0200

    novi random videa razbirash

--- randomVideo.js
+++ randomVideo.js
@@ -4,7 +4,16 @@ function randomVideo() {
         'JYntmqcEYoA?si=j6ko4zS2sTJIBFOH',
         'Sf_iAdMLV7g?si=GRS3khu3GqFOx844',
         '3NXzcZgJmSQ?si=q56BMNA6EepwPd_R',
-        'OnZWBggRHw0?si=JUIZ1c7xvX9Mxh-c'
+        'OnZWBggRHw0?si=JUIZ1c7xvX9Mxh-c',
+        'NosIDDkv2Ls?si=_8G9xDcmn27B0xNq',
+        'hpQmR39-ZWQ?si=gqc8GTflDGV3RHPe',
+        'bBK-CdG3bq4',
+        '_MytdxgCZCM',
+        'SQRvaDMLxMw',
+        '3J_Dnt9OZFs',
+        'xU_OImeuMJ0',
+        'SRiOlOGCx_A',
+        'YS2WRZCnLIQ'
     ];
 
     let videoIndex = Math.floor(Math.random() * videos.length);
@@ -13,4 +22,5 @@ function randomVideo() {
     let html = '<div id="videoPlayer"><iframe width="720" height="480" src="https://www.youtube.com/embed/' + videos[videoIndex] + '?autoplay=1" frameborder="0" allowfullscreen></iframe></div>';
     document.write(html);
 }
+
 randomVideo();
