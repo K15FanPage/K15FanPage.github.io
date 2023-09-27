ff065289a25a57816528e66d6ce4af05f75e9a62
Evgenij31 <109481850+Evgenij31@users.noreply.github.com>
2023-09-27 02:55:51 +0200

    levo desno gaylerija

--- index.js
+++ index.js
@@ -31,3 +31,53 @@ function closeNav() {
   }, 200);
   closeNavBtn.style.display = "none";
 }
+
+//gallery
+let images = document.getElementsByClassName("image");
+let displayImages = document.getElementsByClassName("displayImg");
+const images_length = images.length;
+displayImages[0].style.display = "block";
+
+let latestX = 0;
+
+function displayImg(x) {
+  for (let i = 0; i < images_length; i++) {
+    if (i == x) {
+      displayImages[x].style.animation = "fade .7s";
+      displayImages[x].style.display = "block";
+      document.location.href = "#display";
+      latestX = x;
+    }
+    else {
+      displayImages[i].style.display = "none";
+    }
+  }
+}
+
+function showRight() {
+  let x = latestX;
+  x++;
+
+  if(x >= images_length) {
+    x = 0;
+  }
+
+  console.log(x);
+  displayImg(x);
+
+  latestX = x;
+}
+
+function showLeft() {
+  let x = latestX;
+  x--;
+
+  if(x < 0) {
+    x = images_length-1;
+  }
+
+  console.log(x);
+  displayImg(x);
+
+  latestX = x;
+}
